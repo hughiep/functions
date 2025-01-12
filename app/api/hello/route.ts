2d1bde3044eec5e85e66a59c2344937bc360a656
Hiep <hiepnv.wt@gmail.com>
2025-01-12 11:42:38 +0700

    Add simple function

---
+++ app/api/hello/route.ts
@@ -0,0 +1,6 @@
+export const dynamic = 'force-dynamic' // static by default, unless reading the request
+
+export function GET(request: Request) {
+  console.log(`Request URL: ${request.url}`)
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`)
+}
